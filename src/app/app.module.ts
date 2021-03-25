45e26392d4a30dbe7df8d6e29a9293347623855f
lkbarreto <lkbarreto@uninorte.edu.co>
2021-03-25 15:49:46 -0500

    drawer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { ContentComponent } from './content/content.component';
 
 
 
@@ -17,7 +18,8 @@ import {MatSidenavModule} from '@angular/material/sidenav';
   declarations: [
     AppComponent,
     CardComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
